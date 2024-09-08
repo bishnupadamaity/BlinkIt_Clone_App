ae330740fa1ff8a5ba03b5a73dc19f92f4ac942b
Bishnupada Maity <bishnupdm.1@gmail.com>
2024-09-08 11:15:20 +0530

    add Navigation

--- src/navigation/Navigation.tsx
+++ src/navigation/Navigation.tsx
@@ -3,10 +3,11 @@ import React, { FC } from 'react'
 import {createNativeStackNavigator} from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native';
 import SplashScreen from '@features/auth/SplashScreen';
+import { navigationRef } from '@utils/NavigationUtils';
 const Stack = createNativeStackNavigator();
 const Navigation:FC = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Stack.Navigator initialRouteName='SplashScreen' screenOptions={{headerShown: false}}>
         <Stack.Screen name="SplashScreen" component={SplashScreen} />
       </Stack.Navigator>
