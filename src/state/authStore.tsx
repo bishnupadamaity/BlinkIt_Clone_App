6902c9b524590799f9c028ebe28d274bc1adba0b
Bishnupada Maity <bishnupdm.1@gmail.com>
2024-09-08 11:15:59 +0530

    create storage

---
+++ src/state/authStore.tsx
@@ -0,0 +1,27 @@
+
+import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+import { mmkvStorage } from './storage';
+interface authStore {
+    user: Record<string, any> | null;
+    setUser: (user: any) => void;
+    setCurrentOrder: (order: any) => void;
+    currentOrder: Record<string, any> | null;
+    logOut: () => void;
+}
+
+export const useAuthStore = create<authStore>()(
+    persist(
+        (set, get) => ({
+            user: null,
+            currentOrder: null,
+            setUser: (data) => set({ user: data }),
+            setCurrentOrder: (order) => ({ currentOrder: order }),
+            logOut: ()=>({user:null,currentOrder:null}),
+        }),
+        {
+            name: 'auth-storage',
+            storage: createJSONStorage(() => mmkvStorage),
+        }
+    ),
+)
