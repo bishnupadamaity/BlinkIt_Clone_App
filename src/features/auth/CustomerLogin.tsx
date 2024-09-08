866b382197b5289655c8b7ce72c5aabce2f08e90
Bishnupada Maity <bishnupdm.1@gmail.com>
2024-09-08 11:35:37 +0530

    Splash Screen UI Done

---
+++ src/features/auth/CustomerLogin.tsx
@@ -0,0 +1,13 @@
+import { View, Text, StatusBar } from 'react-native'
+import React from 'react'
+
+const CustomerLogin = () => {
+  return (
+    <View>
+      {/* <StatusBar backgroundColor={'cy'} barStyle={'dark-content'} /> */}
+      <Text style={{color:'black'}}>CustomerLogin</Text>
+    </View>
+  )
+}
+
+export default CustomerLogin
--- src/features/auth/SplashScreen.tsx
+++ src/features/auth/SplashScreen.tsx
@@ -1,12 +1,54 @@
-import { View, Text, StyleSheet, Image } from 'react-native'
-import React from 'react'
-import { Colors } from '@utils/Constants'
+import React, { FC, useEffect } from 'react'
+import { View, StyleSheet, Image, StatusBar, Alert } from 'react-native'
+import GeoLocation from '@react-native-community/geolocation'
 import { screenHeight, screeWidth } from '@utils/Scaling'
 import Logo from '@assets/images/splash_logo.jpeg'
+import { Colors } from '@utils/Constants'
+import { useAuthStore } from '@state/authStore'
+import { tokenStorage } from '@state/storage'
+import { navigate } from '@utils/NavigationUtils'
 
-const SplashScreen = () => {
+GeoLocation.setRNConfiguration({
+    skipPermissionRequests: false,
+    authorizationLevel: 'always',
+    enableBackgroundLocationUpdates: true,
+    locationProvider: 'auto'
+})
+const SplashScreen: FC = () => {
+    
+    const { user, setUser } = useAuthStore();
+
+    const tokenCheck = async () => {
+        const accessToken = tokenStorage.getString('accessToken');
+        const refreshToken = tokenStorage.getString('refreshToken');
+
+        if (accessToken) {
+            
+
+        }
+
+        navigate('CustomerLogin');
+        return false;
+    }
+
+    useEffect(() => {
+        const fetchUserLocation = async () => {
+            try {
+                GeoLocation.requestAuthorization();
+                tokenCheck();
+            } catch (error) {
+                Alert.alert("Sorry we need location service for better experience.")
+            }
+        }
+        const timeOutId = setTimeout(fetchUserLocation, 1000);
+        return () => clearTimeout(timeOutId);
+    },[])
     return (
         <View style={styles.container} >
+            <StatusBar
+                barStyle="dark-content"
+                backgroundColor={Colors.primary}
+            />
             <Image source={Logo} style={styles.logoImage} />
         </View>
     )
