f68d23b1935b366516e9d933048794875d077e15
Bishnupada Maity <bishnupdm.1@gmail.com>
2024-09-11 03:15:41 +0530

    Scroll To Top Feature

--- src/features/dashboard/ProductDashboard.tsx
+++ src/features/dashboard/ProductDashboard.tsx
@@ -1,20 +1,55 @@
-import { View, Text, Animated as RNAnimated, SafeAreaView, StyleSheet } from 'react-native'
-import React, { useEffect, useRef } from 'react'
-import { useAuthStore } from '@state/authStore'
+import {
+  View,
+  Text,
+  Animated as RNAnimated,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { useAuthStore } from '@state/authStore';
 import NoticeAnimation from './NoticeAnimation';
-import { NoticeHeight } from '@utils/Scaling';
+import { NoticeHeight, screenHeight } from '@utils/Scaling';
 import Visuals from './Visuals';
-import { CollapsibleContainer, CollapsibleHeaderContainer, CollapsibleScrollView, withCollapsibleContext } from '@r0b0t3d/react-native-collapsible';
+import {
+  CollapsibleContainer,
+  CollapsibleHeaderContainer,
+  CollapsibleScrollView,
+  useCollapsibleContext,
+  withCollapsibleContext,
+} from '@r0b0t3d/react-native-collapsible';
 import AnimatedHeader from './AnimatedHeader';
 import StickySearchBar from './StickySearchBar';
 import Content from '@components/dashboard/Content';
 import CustomText from '@components/ui/CustomText';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { Fonts } from '@utils/Constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated';
 
-const NOTICE_HEIGHT = -(NoticeHeight + 14)
+const NOTICE_HEIGHT = -(NoticeHeight + 14);
 const ProductDashboard = () => {
+
+
+
+  const { scrollY, expand } = useCollapsibleContext();
+  const previousScroll = useRef<number>(0);
+  const backToTopStyle = useAnimatedStyle(() => {
+    const isScrollingUp =
+      scrollY.value < previousScroll.current && scrollY.value > 180;
+    const opacity = withTiming(isScrollingUp ? 1 : 0, { duration: 300 });
+    const translateY = withTiming(isScrollingUp ? 0 : 10, { duration: 300 });
+    previousScroll.current = scrollY.value;
+
+    return { opacity, transform: [{ translateY }] };
+  });
+
+
+
   const noticePosition = useRef(new RNAnimated.Value(NOTICE_HEIGHT)).current;
+
+  
 
   const slideUp = () => {
     RNAnimated.timing(noticePosition, {
@@ -22,14 +57,14 @@ const ProductDashboard = () => {
       duration: 1200,
       useNativeDriver: false,
     }).start();
-  }
+  };
   const slideDown = () => {
     RNAnimated.timing(noticePosition, {
       toValue: 0,
       duration: 1200,
       useNativeDriver: false,
     }).start();
-  }
+  };
   useEffect(() => {
     slideDown();
     const timeoutId = setTimeout(() => {
@@ -38,30 +73,53 @@ const ProductDashboard = () => {
     return () => {
       clearTimeout(timeoutId);
     };
-  },[])
+  }, []);
   return (
     <NoticeAnimation noticePosition={noticePosition}>
       <>
         <Visuals />
         <SafeAreaView />
+
+        <Animated.View style={[styles.backToTopButton, backToTopStyle]}>
+          <TouchableOpacity
+            style={{ flexDirection: 'row', alignItems: 'center', gap: 6 }}
+            onPress={() => {
+              scrollY.value = 0;
+              expand();
+            }}>
+            <Icon
+              name="arrow-up-circle-outline"
+              color={'#fff'}
+              size={RFValue(12)}
+            />
+            <CustomText
+              variant="h9"
+              style={{ color: '#fff' }}
+              fontFamily={Fonts.SemiBold}>
+              Back to top
+            </CustomText>
+          </TouchableOpacity>
+        </Animated.View>
+
         <CollapsibleContainer style={styles.panelContainer}>
           <CollapsibleHeaderContainer containerStyle={styles.transparent}>
-            <AnimatedHeader showNotice={() => {
-              slideDown();
-              const timeoutId = setTimeout(() => {
-                slideUp();
-              }, 3500);
-              return () => {
-                clearTimeout(timeoutId);
-              };
-              
-            }} />
+            <AnimatedHeader
+              showNotice={() => {
+                slideDown();
+                const timeoutId = setTimeout(() => {
+                  slideUp();
+                }, 3500);
+                return () => {
+                  clearTimeout(timeoutId);
+                };
+              }}
+            />
             <StickySearchBar />
           </CollapsibleHeaderContainer>
           <CollapsibleScrollView
             nestedScrollEnabled
             style={styles.panelContainer}
-            showsVerticalScrollIndicator={false} >
+            showsVerticalScrollIndicator={false}>
             <Content />
             <View style={{ backgroundColor: '#f8f8f8', padding: 20 }}>
               <CustomText
@@ -78,17 +136,29 @@ const ProductDashboard = () => {
             </View>
           </CollapsibleScrollView>
         </CollapsibleContainer>
-        
       </>
     </NoticeAnimation>
-  )
-}
+  );
+};
 const styles = StyleSheet.create({
   panelContainer: {
     flex: 1,
   },
   transparent: {
-    backgroundColor: 'transparent'
-  }
-})
-export default withCollapsibleContext(ProductDashboard)
+    backgroundColor: 'transparent',
+  },
+  backToTopButton: {
+    position: 'absolute',
+    alignSelf: 'center',
+    top: Platform.OS === 'ios' ? screenHeight * 0.18 : 100,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 4,
+    backgroundColor: 'black',
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    zIndex: 999,
+  },
+});
+export default withCollapsibleContext(ProductDashboard);
